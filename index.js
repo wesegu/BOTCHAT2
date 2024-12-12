602369ec245555daa1d600dba948b10659089619
Sergio <infosec.sergio@gmail.com>
2024-12-12 11:00:00 -0300

    commit

--- index.js
+++ index.js
@@ -1,57 +1,63 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-// leitor de qr code
+// Leitor de QR Code
 const qrcode = require('qrcode-terminal');
-const { Client, Buttons, List, MessageMedia } = require('whatsapp-web.js'); // Mudança Buttons
-const client = new Client();
-// serviço de leitura do qr code
-client.on('qr', qr => {
-    qrcode.generate(qr, {small: true});
+const { Client, Buttons, List, MessageMedia } = require('whatsapp-web.js');
+
+// Configuração para Puppeteer com opções de sandbox desativadas
+const client = new Client({
+    puppeteer: {
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    }
 });
-// apos isso ele diz que foi tudo certo
+
+// Serviço de leitura do QR Code
+client.on('qr', qr => {
+    qrcode.generate(qr, { small: true });
+});
+
+// Após a conexão
 client.on('ready', () => {
     console.log('Tudo certo! WhatsApp conectado.');
 });
-// E inicializa tudo 
+
+// Inicializa tudo
 client.initialize();
 
-const delay = ms => new Promise(res => setTimeout(res, ms)); // Função que usamos para criar o delay entre uma ação e outra
+const delay = ms => new Promise(res => setTimeout(res, ms)); // Função para criar o delay entre ações
 
 // Funil
-
 client.on('message', async msg => {
-
     if (msg.body.match(/(menu|Menu|dia|tarde|noite|oi|Oi|Olá|olá|ola|Ola|bom|Bom)/i) && msg.from.endsWith('@c.us')) {
-
         const chat = await msg.getChat();
 
-        await delay(1000); //delay de 3 segundos
-        await chat.sendStateTyping(); // Simulando Digitação
-        await delay(1000); //Delay de 3000 milisegundos mais conhecido como 3 segundos
-        const contact = await msg.getContact(); //Pegando o contato
-        const name = contact.pushname; //Pegando o nome do contato
-        await client.sendMessage(msg.from,'Olá! '+ name.split(" ")[0] + 'Selecione a opção abaixo para agendar seu corte!: \n\n1 - Agendar Corte'); //Primeira mensagem de texto
-        await delay(1000); //delay de 3 segundos
-        await chat.sendStateTyping(); // Simulando Digitação
-        await delay(1000); //Delay de 5 segundos
-    
-        
+        await delay(1000); // Delay de 1 segundo
+        await chat.sendStateTyping(); // Simula digitação
+        await delay(1000);
+        const contact = await msg.getContact(); // Pegando o contato
+        const name = contact.pushname; // Nome do contato
+        await client.sendMessage(
+            msg.from,
+            `Olá! ${name.split(" ")[0]}, selecione a opção abaixo para agendar seu corte!: \n\n1 - Agendar Corte`
+        );
+        await delay(1000);
+        await chat.sendStateTyping();
+        await delay(1000);
     }
-
 
     if (msg.body !== null && msg.body === '1' && msg.from.endsWith('@c.us')) {
         const chat = await msg.getChat();
 
-
-        await delay(1000); //delay de 3 segundos
-        await chat.sendStateTyping(); // Simulando Digitação
+        await delay(1000); // Delay de 1 segundo
+        await chat.sendStateTyping();
         await delay(1000);
         await client.sendMessage(msg.from, 'Link para cadastro: https://calendar.app.google/zz7Qj3Jowi166eCP8');
-        await client.sendMessage(msg.from,'Localização do salão: https://www.google.com/maps?q=37.7749,-122.4194');
-
-
+        await client.sendMessage(msg.from, 'Localização do salão: https://www.google.com/maps?q=37.7749,-122.4194');
     }
-
 });
-app.listen(process.env.PORT || 3000)
+
+// Servidor Express
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Servidor rodando na porta 3000');
+});
